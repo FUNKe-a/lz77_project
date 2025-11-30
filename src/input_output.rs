b201d5d468d8e2f98eab816b28400f6f700178bc
Robertas Kučinskas <robertaskucinskas0@gmail.com>
2025-12-01 00:57:16 +0200

    technically working

--- src/input_output.rs
+++ src/input_output.rs
@@ -2,11 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::{Write, BufReader};
 
-pub fn open_file_buffer(chunk_size: usize) -> BufReader<File> {
-    let args: Vec<String> = env::args().collect();
-
-    let file_path = &args[1];
-
+pub fn open_file_buffer(file_path: &str, chunk_size: usize) -> BufReader<File> {
     let file = match File::open(file_path) {
         Ok(file) => file,
         Err(error) => panic!("Problem opening file: {error:?}"),
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,59 @@
-// use rayon::prelude::*;
-// use std::sync::mpsc;
-use std::env;
-use std::fs::{self, File};
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+use rayon::prelude::*;
+use std::fs;
 
 mod input_output;
 mod lz_77;
 
-const CHUNK_SIZE: usize = 4096;
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(value_enum)]
+    mode: Mode,
+    #[arg(short, long)]
+    output: String,
+    #[arg(short, long)]
+    input: String,
+    #[arg(short, long, default_value_t = 64 * 1024)]
+    chunk_size: usize,
+}
 
-#[derive(Parser, Debug)]
-struct Args {
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    Singlethreaded,
+    Multithreaded,
+    Decompress,
 }
 
 fn main() {
-    let mut file_buffer = input_output::open_file_buffer(CHUNK_SIZE);
-    let mut buffer = [u8::MIN; CHUNK_SIZE];
-    let mut chunk_id = 0;
+    let cli = Cli::parse();
 
-    let args: Vec<String> = env::args().collect();
+    let data = fs::read(&cli.input).unwrap();
+    let chunks: Vec<&[u8]> = data.chunks(cli.chunk_size).collect();
+    let mut res: Vec<u8> = Vec::new();
+    match cli.mode {
+        Mode::Singlethreaded => {
+            // let mut reader = input_output::open_file_buffer(&cli.input, cli.io_chunk_size);
+            // let mut buffer = vec![0u8; cli.io_chunk_size];
 
-    let data = fs::read(&args[1]).unwrap();
+            for chunk in chunks {
+                let mut comp_chunk;
+                comp_chunk = lz_77::compress(&chunk);
+                res.append(&mut comp_chunk);
+            }
+        },
+        Mode::Multithreaded => {
+            let comp_chunks = chunks
+                .par_iter()
+                .map(|chunk| lz_77::compress(chunk))
+                .collect::<Vec<Vec<u8>>>();
 
-    let compressed = lz_77::compress(&data);
+            for chunk in comp_chunks { res.extend(chunk); }
+        },
+        Mode::Decompress => {
+            res = lz_77::decompress(&data);
+        },
+    }
 
-    input_output::output_to_file(&compressed, "/home/LinRob/KTU/Multithreading_code/inzinerinis_projektas/inzinerinis_projektas/results/pog.lz77");
-
-    let data_comp = fs::read("/home/LinRob/KTU/Multithreading_code/inzinerinis_projektas/inzinerinis_projektas/results/pog.lz77").unwrap();
-
-    let decomp = lz_77::decompress(&data_comp);
-
-    input_output::output_to_file(&decomp, "/home/LinRob/KTU/Multithreading_code/inzinerinis_projektas/inzinerinis_projektas/results/pog_dec.txt");
-
-    // while let Ok(n) = file_buffer.read(&mut buffer) {
-    //     if n == 0 {
-    //         break;
-    //     }
-    //
-    //     lz_77::compress(&buffer);
-    // }
+    input_output::output_to_file(&res, &cli.output);
 }
